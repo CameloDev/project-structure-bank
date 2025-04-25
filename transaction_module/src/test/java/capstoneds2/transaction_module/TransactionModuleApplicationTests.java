7a52ed5d2c57a86214e5e4628b6c0f8802f7eb3d
CameloDev <prozinffzin03051@gmail.com>
2025-04-25 15:32:19 -0400

    Adding arquitetura MVC

---
+++ transaction_module/src/test/java/capstoneds2/transaction_module/TransactionModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package capstoneds2.transaction_module;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionModuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
