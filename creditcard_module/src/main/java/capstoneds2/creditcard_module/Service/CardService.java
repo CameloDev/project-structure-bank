b9631693dbc50d08a50120143fe80616066c69b6
CameloDev <freefireops@outlook.com>
2025-05-02 18:04:43 -0400

    adding my work to kill me

--- creditcard_module/src/main/java/capstoneds2/creditcard_module/Service/CardService.java
+++ creditcard_module/src/main/java/capstoneds2/creditcard_module/Service/CardService.java
@@ -7,6 +7,8 @@ import capstoneds2.creditcard_module.Repository.CartaoRepository;
 import lombok.ToString;
 
 import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
 import java.util.Random;
 
 public class CardService {
@@ -52,6 +54,42 @@ public class CardService {
         } while (cartaoRepository.existsByNumero(numero));
 
         return numero;
+    }
+    // READ (por ID)
+    public Optional<Cartao> buscarCartaoPorId(Long id) {
+        return cartaoRepository.findById(id);
+    }
+
+    // READ (todos)
+    public List<Cartao> listarTodosCartoes() {
+        return cartaoRepository.findAll();
+    }
+
+    // UPDATE
+    public Optional<Cartao> atualizarCartao(Long id, Cartao novosDados) {
+        return cartaoRepository.findById(id).map(cartao -> {
+            cartao.setNome_impresso(novosDados.getNome_impresso());
+            cartao.setNumero(novosDados.getNumero());
+            cartao.setCvv(novosDados.getCvv());
+            cartao.setData_validade(novosDados.getData_validade());
+            cartao.setLimite_total(novosDados.getLimite_total());
+            cartao.setLimite_disponivel(novosDados.getLimite_disponivel());
+            cartao.setBandeiraCartao(novosDados.getBandeiraCartao());
+            cartao.setStatusCartao(novosDados.getStatusCartao());
+            cartao.setAprovacao_automatica(novosDados.getAprovacao_automatica());
+            cartao.setEh_adicional(novosDados.getEh_adicional());
+            cartao.setData_emissao(novosDados.getData_emissao());
+            return cartaoRepository.save(cartao);
+        });
+    }
+
+    // DELETE
+    public boolean deletarCartaoPorId(Long id) {
+        if (cartaoRepository.existsById(id)) {
+            cartaoRepository.deleteById(id);
+            return true;
+        }
+        return false;
     }
 
 
