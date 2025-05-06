983e392a7e7535e5c7cbf64ef0c43be7ace602e6
CameloDev <freefireops@outlook.com>
2025-05-06 15:50:50 -0400

    Added BL-010 to main

--- creditcard_module/src/main/java/capstoneds2/creditcard_module/Repository/CartaoRepository.java
+++ creditcard_module/src/main/java/capstoneds2/creditcard_module/Repository/CartaoRepository.java
@@ -1,11 +1,15 @@
 package capstoneds2.creditcard_module.Repository;
 
 import capstoneds2.creditcard_module.Model.Cartao;
+import capstoneds2.creditcard_module.Model.Enums.StatusCartao;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface CartaoRepository extends JpaRepository<Cartao, Long> {
 
     boolean existsByNumero(String numero);
 
+    List<Cartao> findByStatusCartao(StatusCartao statusCartao);
 
 }
